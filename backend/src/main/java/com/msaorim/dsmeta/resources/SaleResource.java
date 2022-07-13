a0c9e820d9df2a82876fe0fec46ae29c80ada6e9
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-13 15:24:21 -0300

    Date select

--- backend/src/main/java/com/msaorim/dsmeta/resources/SaleResource.java
+++ backend/src/main/java/com/msaorim/dsmeta/resources/SaleResource.java
@@ -1,12 +1,13 @@
 package com.msaorim.dsmeta.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msaorim.dsmeta.entities.Sale;
@@ -19,15 +20,16 @@ public class SaleResource {
 	@Autowired
 	private SaleService saleService;
 
-	//@GetMapping
-	//public List<Sale> findAll() {
-	//	return saleService.findAll();
-	//}
-
 	@GetMapping
-	public ResponseEntity<List<Sale>> findAll() {
-		return ResponseEntity.ok().body(saleService.findAll());
+	public Page<Sale> findAll(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageable) {
+		return saleService.findAll(minDate, maxDate, pageable);
 	}
+
+	// @GetMapping
+	// public ResponseEntity<List<Sale>> findAll() {
+	// return ResponseEntity.ok().body(saleService.findAll());
+	// }
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Sale> findById(@PathVariable long id) {
--- backend/src/main/java/com/msaorim/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/msaorim/dsmeta/services/SaleService.java
@@ -1,9 +1,13 @@
 package com.msaorim.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.msaorim.dsmeta.entities.Sale;
@@ -15,8 +19,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findAll(){
-		return saleRepository.findAll();
+	public Page<Sale> findAll(String minDate, String maxDate, Pageable pageable){
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return saleRepository.findAll(min, max, pageable);
 	}
 
 	public Sale findById(long id) {
