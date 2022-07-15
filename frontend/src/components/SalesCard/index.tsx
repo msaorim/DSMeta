093a2fd767e4570294d5982fdc9c122d81e60ace
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-15 11:39:35 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -76,7 +76,7 @@ function SalesCard() {
                                     <td>R$ {item.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={item.id} />
                                         </div>
                                     </td>
                                 </tr>
